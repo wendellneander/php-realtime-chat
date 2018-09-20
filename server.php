6d3c98b905d1a279b06ad438789cc332dff2e0c7
Wendell Neander <neanderdev@gmail.com>
2018-09-20 00:03:29 -0300

    refatoring

---
+++ server.php
@@ -0,0 +1,19 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+use App\Classes\Chat;
+use Ratchet\Http\HttpServer;
+use Ratchet\Server\IoServer;
+use Ratchet\WebSocket\WsServer;
+
+$server = IoServer::factory(
+    new HttpServer(
+        new WsServer(
+            new Chat()
+        )
+    ),
+    8080
+);
+
+$server->run();
